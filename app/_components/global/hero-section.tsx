59a345397fa3738f50dfb017b0cbe7c65341add2
nnagaich <neelabhnagaich@gmail.com>
2024-05-01 09:49:29 +0530

    adding background to navbar

--- app/_components/global/hero-section.tsx
+++ app/_components/global/hero-section.tsx
@@ -37,19 +37,15 @@ export default function HeroSection() {
       </div>
       <div className="overlay absolute h-full w-full bg-black bg-opacity-50 top-0 left-0">
       </div>
-      <div className="hero-section__info absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="hero-section__info absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
         <h3 className="text-2xl">Connecting clients with the world through tailor-made itineraries</h3>
       </div>
 
-      <div className="hero-section__info absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h3 className="text-lg font-semibold tracking-widest">ADVENTURE AWAITS</h3>
+      <div className="hero-section__info absolute top-[80svh] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+        <h3 className="text-base font-semibold tracking-widest">ADVENTURE AWAITS</h3>
       </div>
-      <a href="#/" className="hero-section__info absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="m461.49,248.51,394,181a12,12,0,0,0-17,17l46.52,46.51h60a12,12,0,0,0,0,24h424.53l377,316a12,12,0,0,0,17,17l67.5-67.5a12,12,0,0,0,461.49,248.51z"></path>
-          </svg>
-        </div>
+      <a href="#/" className="hero-section__info absolute arrow-down top-[80svh]">
+          <img src="/down-arrow-svgrepo-com.svg" alt="arrow Down" className="w-28 h-28 text-white" /> 
       </a>
     </div>
   )
--- app/_components/global/navigation.tsx
+++ app/_components/global/navigation.tsx
@@ -1,14 +1,42 @@
+'use client'
 import Link from 'next/link'
+import React, { useEffect, useRef } from 'react';
 
 const NavigationHeader = () => {
+  const headerRef  = useRef(null); 
+
+useEffect(() => {
+  let wasScrolled = window.pageYOffset > 20;
+
+  const handleScroll = () => {
+    const isScrolled = window.pageYOffset > 20;
+    if (isScrolled !== wasScrolled) {
+      wasScrolled = isScrolled;
+      if (isScrolled) {
+          console.log("heelo")
+        headerRef.current.classList.add("bg-black");
+        headerRef.current.style.backgroundColor = 'rgba(0,0,0,0.5)';
+      } else {
+        headerRef.current.style.backgroundColor = 'transparent';
+      }
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+  return () => {
+    window.removeEventListener("scroll", handleScroll);
+  };
+}, []);
   const links = [
     { href: '/', label: 'Home' },
     { href: '/docs', label: 'Docs' },
     { href: '/todos', label: 'Todos' },
     { href: '/blogs', label: 'Blogs' },
   ];
+
   return (
-    <header className="mb-5 absolute z-10 text-white w-full">
+    <header ref={headerRef} className="p-[20px] fixed z-10 text-white w-full">
       <nav className='container mx-auto'>
         <ul className="flex items-center">
           {links.map(({ href, label }) => (
