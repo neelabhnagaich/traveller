40cb3b9b8e54934a78fb5993b20e8b30bea7d460
nnagaich <neelabhnagaich@gmail.com>
2024-05-10 19:15:27 +0530

    added logo and mobile menu

--- app/_components/global/hero-section.tsx
+++ app/_components/global/hero-section.tsx
@@ -31,21 +31,23 @@ export default function HeroSection() {
   }, [])
 
   return (
-    <div ref={heroRef} className="hero-section h-svh relative text-white" style={{ height: '100svh' }}>
+    <div ref={heroRef} className="hero-section h-svh relative text-white min-h-[640px]" style={{ height: '100svh'}}>
       <div className="hero-section_bg h-full w-full" >
         <video ref={bgRef} className="object-cover w-full h-full" src='../bg_video.mp4' autoPlay loop muted></video>
       </div>
       <div className="overlay absolute h-full w-full bg-black bg-opacity-50 top-0 left-0">
       </div>
-      <div className="hero-section__info absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+      <div className="hero-section__info absolute top-[20%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center container mx-auto px-4 flex justify-center">
+        <img src="/RI2-removebg-preview.png" alt="logo" className="w-56 h-56" />
+      </div>
+      <div className="hero-section__info absolute top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center container mx-auto px-4">
         <h3 className="text-2xl">Connecting clients with the world through tailor-made itineraries</h3>
       </div>
-
-      <div className="hero-section__info absolute top-[80svh] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+      <div className="hero-section__info absolute top-[80%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center container mx-auto px-4">
         <h3 className="text-base font-semibold tracking-widest">ADVENTURE AWAITS</h3>
       </div>
-      <a href="#/" className="hero-section__info absolute arrow-down top-[80svh]">
-          <img src="/down-arrow-svgrepo-com.svg" alt="arrow Down" className="w-28 h-28 text-white" /> 
+      <a href="#/" className="hero-section__info absolute arrow-down top-[85%]">
+          <img src="/down-arrow-svgrepo-com.svg" alt="arrow Down" className="w-10 h-10 text-white" /> 
       </a>
     </div>
   )
--- app/_components/global/navigation.tsx
+++ app/_components/global/navigation.tsx
@@ -1,9 +1,12 @@
 'use client'
 import Link from 'next/link'
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const NavigationHeader = () => {
   const headerRef = useRef(null);
+
+  const menuOpener = useRef(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     let wasScrolled = window.pageYOffset > 20;
@@ -24,6 +27,7 @@ const NavigationHeader = () => {
       }
     };
 
+
     handleScroll();
 
     window.addEventListener("scroll", handleScroll);
@@ -32,6 +36,31 @@ const NavigationHeader = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+
+  useEffect(() => {
+
+    window.addEventListener("click", bodyClick)
+
+    return () => {
+
+      window.removeEventListener("click", bodyClick);
+    };
+    
+  }, [open]);
+
+  const bodyClick = (e) => {
+    console.log('body click', open);
+
+    if(e.target.closest('button') !== menuOpener.current){
+
+    console.log('body click 22');
+      if(open){
+        setOpen(false);
+      }
+    }
+  }
+
   const links = [
     { href: '/', label: 'Home' },
     { href: '/docs', label: 'Docs' },
@@ -39,16 +68,35 @@ const NavigationHeader = () => {
     { href: '/blogs', label: 'Blogs' },
   ];
 
+  const openMobileMenu = () => {
+    setOpen(!open);
+  }
+
+
+  console.log('open', open);
+
   return (
     <header ref={headerRef} className="p-[20px] fixed z-10 text-white w-full">
       <nav className='container mx-auto'>
-        <ul className="flex items-center">
+        <ul className="md:flex items-center hidden">
           {links.map(({ href, label }) => (
             <li key={href} className="px-4 ">
               <Link href={href}>{label}</Link>
             </li>
           ))}
         </ul>
+        <div className='sm:flex md:hidden'>
+          <button ref={menuOpener} type="button" onClick={openMobileMenu} className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:outline-none hover:ring-2 hover:ring-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5 fill-current text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"  />
+            </svg>
+          </button>          
+          {open && 
+            <div className={`h-[100vh] w-[50%] bg-black absolute top-0 left-0 ${open ? '': 'hidden'}`}>
+            </div>
+          }
+        </div>
       </nav>
     </header>
   )
